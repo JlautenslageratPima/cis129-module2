e6ca7e1a0940f41da06fa766462ea6ba8a4dd397
JlautenslageratPima <160447485+JlautenslageratPima@users.noreply.github.com>
2024-02-21 15:49:33 -0800

    Update cis129_lab02_shortStory.py
    Updated with short story and time spacing

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,17 @@
-print('Hello, World!')
+# Author: Joshua Lautenslager
+# My first Python lab in my CIS129 course
+# Prints a neverending short story about rats
+import time
+crazy = True
+while crazy == True:
+  print('Crazy? I Was Crazy Once.')
+  time.sleep(1)
+  print('They Locked Me In A Room.')
+  time.sleep(1)
+  print('A Rubber Room.')
+  time.sleep(1)
+  print('A Rubber Room With Rats.')
+  time.sleep(1)
+  print('And Rats Make Me Crazy.')
+  time.sleep(1)
+
